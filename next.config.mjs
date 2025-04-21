c374d79a26daede833f3d441de1690330530576c
Fabio Farias  Figueiroa <fabiofariasf@gmail.com>
2025-04-21 13:04:44 -0300

    melhorias de segurança

--- next.config.mjs
+++ next.config.mjs
@@ -19,12 +19,6 @@ const nextConfig = {
       },
     ]
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
     unoptimized: true,
   },
